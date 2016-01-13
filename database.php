0d2f7a2309950f584653931e07dcb635ff1ebe8c
spraveenitpro <praveen.selvasekaran@automattic.com>
2016-01-13 23:56:52 +0530

    Created Connection to Quizzer Database

--- database.php
+++ database.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: praveen
- * Date: 11/01/16
- * Time: 11:30 PM
- */
+//Create Connection Credentials
+
+$db_host = 'localhost';
+$db_name = 'quizzer';
+$db_user = 'root';
+$db_password = 'root';
+
+//Create Mysqli Object
+
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if ($mysqli->connect_error) {
+    printf("Connect Failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+
+?>
