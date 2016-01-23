201e45974f5994e53dd48c5079f0fc68cf094b2c
spraveenitpro <praveen.selvasekaran@automattic.com>
2016-01-23 11:42:45 +0530

    Added Ability to Enter new questions for Quiz

--- add.php
+++ add.php
@@ -1,3 +1,75 @@
+<?php
+include 'database.php';
+
+if (isset($_POST['submit'])) {
+    //Get Post Variables
+
+    $question_number = $_POST['question_number'];
+    $question_text = $_POST['question_text'];
+    $correct_choice = $_POST['correct_choice'];
+
+    //Choices Array
+
+    $choices = array();
+
+
+    $choices[1] = $_POST['choice1'];
+    $choices[2] = $_POST['choice2'];
+    $choices[3] = $_POST['choice3'];
+    $choices[4] = $_POST['choice4'];
+    $choices[5] = $_POST['choice5'];
+    $correct_choice = $_POST['correct_choice'];
+
+
+    //Question Query
+
+    $query = "INSERT into `questions` (question_number, text) VALUES ('$question_number', '$question_text')";
+
+    //Run query
+
+    $insert_row = $mysqli->query($query) or die ($mysqli->error .__LINE__);
+
+    //Validate row
+
+    if ($insert_row) {
+        foreach ($choices as $choice => $value) {
+            if($value != '') {
+                if($correct_choice == $choice) {
+                    $is_correct = 1;
+                } else {
+                    $is_correct = 0;
+                }
+                // Choice query
+                $query = "INSERT INTO `choices` (question_number, is_correct, text)
+                                VALUES('$question_number', '$is_correct', '$value')";
+
+                // Run query
+                $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+                // Validate insert
+                if ($insert_row) {
+                    continue;
+                } else {
+                    die('Error: ('.$mysqli->errno . ') '. $mysqli->error);
+                }
+            }
+        }
+        $msg = 'Question has been added';
+    }
+}
+
+/*
+* Get Total questions
+*/
+$query = "select * from questions";
+//Get Results
+$results = $mysqli->query($query) or die($mysqli->error.__LINE__);
+$total = $results->num_rows;
+$current = $total + 1;
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,10 +89,19 @@
     <div class="container">
 
         <h2>Add a Question</h2>
+
+        <?php
+        if (isset($msg)) {
+
+            echo '<p>'.$msg.'</p>';
+
+        }
+
+        ?>
         <form action="add.php" method="post">
             <p>
                 <label>Question Number</label>
-                <input type="number" name="question_number" />
+                <input type="number" name="question_number" value="<?php echo $current ?>" />
             </p>
             <p>
                 <label>Question Text</label>
@@ -55,7 +136,7 @@
 
                 <input type="submit" name="submit" />
             </p>
-            
+
         </form>
     </div>
 </main>
